1e02caadfa8a530d8b9c5510a60441aae5e58708
Josephondielo <josebonagain@gmail.com>
2025-08-16 10:04:25 +0300

    synchronized

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,22 +1,3 @@
-// import React from "react";
-
-// function Item({ item }) {
-//   return (
-//     <li className={item.isInCart ? "in-cart" : ""}>
-//       <span>{item.name}</span>
-//       <span className="category">{item.category}</span>
-//       <button className={item.isInCart ? "remove" : "add"}>
-//         {item.isInCart ? "Remove From" : "Add to"} Cart
-//       </button>
-//       <button className="remove">Delete</button>
-//     </li>
-//   );
-// }
-
-// export default Item;
-
-
-// src/components/Item.js
 import React from "react";
 
 function Item({ item, onUpdateItem, onDeleteItem }) {
